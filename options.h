0fe457689163ede4d3c72b7bf7b6f096b40b438c
lpk <605534244@qq.com>
2023-10-12 21:31:23 +0800

    changed path

--- options.h
+++ options.h
@@ -3,7 +3,7 @@
 
 #define DEBUG 0
 #define PINNING 1
-#define PATH "/mnt/ssd%lu/kvell/slab-%d-%lu-%lu"
+#define PATH "/home/lpk/mnt/ssd%lu/kvell/slab-%d-%lu-%lu"
 
 /* In memory structures */
 #define RBTREE 0
