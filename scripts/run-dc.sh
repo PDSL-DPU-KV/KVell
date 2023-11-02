2a0407fbf43fe047036941806197da133e651fb9
lpk <605534244@qq.com>
2023-11-02 14:38:32 +0000

    modified run script

--- scripts/run-dc.sh
+++ scripts/run-dc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mainDir="${scriptDir}/.."
-tcmalloc="env LD_PRELOAD=${HOME}/gperftools/.libs/libtcmalloc.so "
+tcmalloc="env LD_PRELOAD=${HOME}/repo/gperftools/.libs/libtcmalloc.so "
 
 #/!\ This script will NOT work if you modify main.c
 
@@ -9,7 +9,7 @@ tcmalloc="env LD_PRELOAD=${HOME}/gperftools/.libs/libtcmalloc.so "
 #Run YCSB A B C (D = A, F =B)
 #4 workers per disk, 4 load injectors
 #
-rm -f /mnt/ssd*/kvell/*
+rm -f /home/lpk/mnt/ssd*/kvell/*
 
 cp ${mainDir}/main.c ${mainDir}/main.c.bak
 # cat ${mainDir}/main.c | perl -pe 's://.nb_load_injectors = 4:.nb_load_injectors = 4:' | perl -pe 's:[^/].nb_load_injectors = 12: //.nb_load_injectors = 12:' | perl -pe 's:[^/]ycsb_e_uniform,: //ycsb_e_uniform,:' | perl -pe 's://ycsb_a_uniform,:ycsb_a_uniform,:' | perl -pe 's://ycsb_a_zipfian,:ycsb_a_zipfian,:' > ${mainDir}/main.c.tmp
@@ -18,7 +18,7 @@ mv ${mainDir}/main.c.tmp ${mainDir}/main.c
 make -C ${mainDir} -j
 
 echo "Run 1"
-${tcmalloc} ${mainDir}/main 3 8 | tee log_ycsb_1
+${tcmalloc} ${mainDir}/main 1 8 | tee log_ycsb_1
 
 # echo "Run 2"
 # ${tcmalloc} ${mainDir}/main 1 1 | tee log_ycsb_2
